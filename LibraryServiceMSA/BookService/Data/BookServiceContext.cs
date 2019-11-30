1ec29e26a791014994aa61fc07522b17e8134087
Nichlas Bjorndal <s173086@student.dtu.dk>
2019-11-30 15:34:00 +0100

    initial bookservice - docker container can't connect to sql server

---
+++ LibraryServiceMSA/BookService/Data/BookServiceContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookService.Models
+{
+    public class BookServiceContext : DbContext
+    {
+        public BookServiceContext (DbContextOptions<BookServiceContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BookService.Models.Book> Book { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var book1 = new Book
+            {
+                Id = 1,
+                ISBN = "1234",
+                Title = "Book A",
+                AuthorId = 1
+            };
+
+            var book2 = new Book
+            {
+                Id = 2,
+                ISBN = "1234",
+                Title = "Book B",
+                AuthorId = 2
+            };
+
+            var book3 = new Book
+            {
+                Id = 3,
+                ISBN = "1234",
+                Title = "Book Z",
+                AuthorId = 3
+            };
+
+            modelBuilder.Entity<Book>(b =>
+            {
+                b.HasData(
+                    book1, book2, book3
+                );
+            });
+        }
+    }
+}
---
+++ LibraryServiceMSA/BookService/Migrations/20191130141237_init-books.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookService.Migrations
+{
+    public partial class initbooks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ISBN = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true),
+                    AuthorId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Book",
+                columns: new[] { "Id", "AuthorId", "ISBN", "Title" },
+                values: new object[] { 1, 1, "1234", "Book A" });
+
+            migrationBuilder.InsertData(
+                table: "Book",
+                columns: new[] { "Id", "AuthorId", "ISBN", "Title" },
+                values: new object[] { 2, 2, "1234", "Book B" });
+
+            migrationBuilder.InsertData(
+                table: "Book",
+                columns: new[] { "Id", "AuthorId", "ISBN", "Title" },
+                values: new object[] { 3, 3, "1234", "Book Z" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book");
+        }
+    }
+}
