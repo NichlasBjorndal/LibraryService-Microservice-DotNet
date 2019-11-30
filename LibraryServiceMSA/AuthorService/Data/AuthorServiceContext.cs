3992a72c77b950bd5a42435c89b8ac07bad88ea7
Nichlas Bjorndal <s173086@student.dtu.dk>
2019-11-30 17:59:28 +0100

    added initial 4 microservices

---
+++ LibraryServiceMSA/AuthorService/Data/AuthorServiceContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthorService.Models
+{
+    public class AuthorServiceContext : DbContext
+    {
+        public AuthorServiceContext (DbContextOptions<AuthorServiceContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AuthorService.Models.Author> Author { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            var author1 = new Author
+            {
+                Id = 1,
+                FirstName = "Edger Allan",
+                LastName = "Poe"
+            };
+
+            var author2 = new Author
+            {
+                Id = 2,
+                FirstName = "Thomas",
+                LastName = "Edison"
+            };
+
+            var author3 = new Author
+            {
+                Id = 3,
+                FirstName = "H.C",
+                LastName = "Andersen"
+            };
+
+            modelBuilder.Entity<Author>().HasData(
+                author1, author2, author3
+            );
+        }
+    }
+}
---
+++ LibraryServiceMSA/LoanService/Models/Loan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoanService.Models
+{
+    public class Loan
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int BookId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool Active { get; set; }
+    }
+}
