aa064295e7232946fdc05bfa7704e7bfdfee4bfd
Nichlas Bjorndal <s173086@student.dtu.dk>
2019-12-13 20:29:19 +0100

    updated loan PK to autogen

--- LibraryServiceMSA/LoanService/Models/Loan.cs
+++ LibraryServiceMSA/LoanService/Models/Loan.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace LoanService.Models
 {
     public class Loan
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public int UserId { get; set; }
         public int BookId { get; set; }
